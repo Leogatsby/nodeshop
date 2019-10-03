f8f82c906082c0237afb35879d74db795342ed79
leo <100k1@naver.com>
2019-10-03 11:43:32 +0900

    add password bcryptjs
    패스워드 암호화 시키기

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,29 +1,40 @@
 const express = require("express");
 const router = express.Router();
 const userModel = require("../model/users");
-
+const bcrypt = require("bcryptjs");
 
 // 회원가입 => 회원 정보 등록 던져줘야 하니깐 포스트다.
 router.post("/register" ,(req,res) => {
-    const user = new userModel({
-        email : req.body.email,
-        password : req.body.password
+
+    bcrypt.hash(req.body.password, 10, (err,hash) => {
+        if(err){
+            return res.status(500).json({
+                error: err
+            });
+        }else{
+            const user = new userModel({
+                email : req.body.email,
+                password : hash     //req.body.password가 암호화 되었으니 hash로 친다.
+            });
+        
+            user
+             .save()
+             .then(result => {
+                 res.status(200).json({
+                     msg : "user created, 회원가입 되셨습니다.",
+                     userInfo: result
+                 });
+             })
+             .catch(err => {
+                 res.status(500).json({
+                     error : err
+                 });
+             });
+        }
     });
 
-    user
-     .save()
-     .then(result => {
-         res.status(200).json({
-             msg : "user created, 회원가입 되셨습니다.",
-             userInfo: result
-         });
-     })
-     .catch(err => {
-         res.status(500).json({
-             error : err
-         });
-     });
 
+ 
 });
 
 // 로그인  => 이메일과 패스워드를 던져줘야 하니깐 포스트다.
