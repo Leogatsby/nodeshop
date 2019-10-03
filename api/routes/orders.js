51fb644242df08361d31906a0fcc789ce7d26dc7
leo <100k1@naver.com>
2019-10-03 10:14:49 +0900

    오더 포스팅
    오더 등록 하는 과정

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const orderModel = require("../model/orders");
+const productModel = require("../model/products");
 
 // order 데이터 불러오기
 
@@ -12,9 +14,41 @@ router.get("/", (req, res) => {
 // order 데이터 생성하기
 
 router.post("/", (req,res) => {
-    res.status(200).json({
-        msg: "order post"
-    });
+    productModel
+        .findById(req.body.productID) //아이디가 있는지 없는지를 , 제품이 있는지 없는지를 먼저 찾아야한다.
+        .then(product => {
+            if(!product){
+                return res.status(404).json({
+                    msg : "제품 없어 !!!"
+                });
+            }else{
+                const order = new orderModel({
+                    product : req.body.productID,
+                    quantity : req.body.quantity
+                });
+                return order.save();
+            }
+        })             //
+        .then(result => {
+            res.status(200).json({
+                msg : "오더가 저장되었습니다.",
+                createdOrder1 : result,
+                createdOrder2 : {
+                    product : result.product,
+                    quantity : result.quantity,
+                    _id : result._id
+                },
+                request : {
+                    type : "GET",
+                    url: "http://localhost:3000/orders/"+result._id
+                }
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error : err
+            });
+        });
 });
 // order 데이터 수정하기
 router.patch("/",(req,res) => {
